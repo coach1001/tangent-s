8977b0a3c91942f41a8d236b492ede6dfafb3273
Francois Weber <coach1001@gmail.com>
2018-03-01 22:20:27 +0200

    Custom Data Table - Filter Cont.

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -7,37 +7,38 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DataTableComponent implements OnInit {
   @Input() columns:any;
-  @Input() rows:any;  
-  
-  flattenedRows:any = [];  
+  @Input() rows:any;
+
+  flattenedRows:any = [];
   filteredRows:any = [];
   filters:any = [];
-  dropdown:any = [];  
+  dropdown:any = [];
 
   constructor() { }
-  
-  ngOnInit() {    
+
+  ngOnInit() {
     this.rows.map((row) => {
       let new_row = {};
       this.columns.map((col)=> {
         new_row[col.prop] = this.getValue(row, col.prop)
       })
       this.flattenedRows.push(new_row);
-    })        
+    })
     this.filteredRows = this.flattenedRows.slice(0);
   }
 
-  toggleDropdown(i:number) {  
-    this.dropdown[i] = !this.dropdown[i];    
+  toggleDropdown(i:number) {
+    this.dropdown[i] = !this.dropdown[i];
   }
 
   addFilter() {
     this.dropdown.push(false);
-    this.filters.push({prop: 'No Filter', name: 'No Filter', value: ''});    
+    this.filters.push({prop: 'All', name: 'All', value: ''});
   }
 
   removeFilter(index:number) {
     this.filters.splice(index,1);
+    this.filter_();
   }
 
   setFilterProp(prop:string, name:string, i:number) {
@@ -46,15 +47,37 @@ export class DataTableComponent implements OnInit {
     this.dropdown[i] = false;
   }
 
-  filter_() {    
+  filter_() {
     let rowsCopy = this.flattenedRows.slice(0);
-    this.filters.map((_filter) => {
-      rowsCopy = rowsCopy.filter((row) => {
-        this.columns.map((col) => {
-          
-        })        
-      })
-    })
+    let rowsToKeep = [];
+    for(var i=0; i < this.filters.length; i++) {
+    //FILTERS
+
+      for(var j=0; j < rowsCopy.length; j++) {
+        //ROWS
+        for(var k=0; k < this.columns.length; k++) {
+        //COLS
+          if(this.filters[i].prop === 'All') {
+            if(this.filters[i].value === '') {
+              //removeRow = false;
+              rowsToKeep.push(rowsCopy[j]);break;
+            } else {
+              let cellValue = rowsCopy[j][this.columns[k].prop].toLowerCase();
+              let filterValue = this.filters[i].value.toLowerCase();
+              if( cellValue.indexOf(filterValue) !== -1) {
+                rowsToKeep.push(rowsCopy[j]);
+                break;
+              }
+            }
+          }
+        //COLS
+        }
+      //ROWS
+      }
+      rowsCopy = rowsToKeep.slice(0);
+      rowsToKeep = [];
+    //FILTERS
+    }
     this.filteredRows = rowsCopy.slice(0);
   }
 
